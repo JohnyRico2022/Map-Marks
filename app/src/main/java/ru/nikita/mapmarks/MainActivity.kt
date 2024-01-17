06466c0c51d6588b7da2f917d5a6fd548edbc1e5
Nikita Konstantinov <110055310+JohnyRico2022@users.noreply.github.com>
2024-01-17 13:55:47 +0300

    add MapKit in progect

--- app/src/main/java/ru/nikita/mapmarks/MainActivity.kt
+++ app/src/main/java/ru/nikita/mapmarks/MainActivity.kt
@@ -2,10 +2,47 @@ package ru.nikita.mapmarks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandex.mapkit.MapKitFactory
+
+
+import ru.nikita.mapmarks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setApiKey(savedInstanceState)
+        MapKitFactory.initialize(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun setApiKey(savedInstanceState: Bundle?) {
+        val haveApiKey = savedInstanceState?.getBoolean("haveApiKey") ?: false
+        if (!haveApiKey) {
+            MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("haveApiKey", true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapview.onStart()
+    }
+
+    override fun onStop() {
+        binding.mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    companion object {
+        const val MAPKIT_API_KEY = "c439084a-3fdb-4fe3-903f-e0d2ee642d43"
     }
 }
